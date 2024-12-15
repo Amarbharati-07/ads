2fb0521953f6c57bfbbeba73f768dd5770a480da
Amar Shivbasant Bharati <146354740+Amarbharati-07@users.noreply.github.com>
2024-12-16 05:06:55 +0530

    Update GraphBFS.java

--- GraphBFS.java
+++ GraphBFS.java
@@ -1,71 +1,86 @@
 import java.util.*;
 
 public class GraphBFS {
-    static final int NODE = 6; // Number of nodes in the graph
-    static int[][] graph = {
-        {0, 1, 1, 1, 0, 0},
-        {1, 0, 0, 1, 1, 0},
-        {1, 0, 0, 1, 0, 1},
-        {1, 1, 1, 0, 1, 1},
-        {0, 1, 0, 1, 0, 1},
-        {0, 0, 1, 1, 1, 0}
-    };
+    static final int MAX = 20;
+    static int[][] adj = new int[MAX][MAX];
+    static boolean[] visited = new boolean[MAX];
+    static int n;
 
-    // Node class to represent a vertex
-    static class Node {
-        int val;     // Vertex index
-        boolean visited; // Visited state
+    public static void createGraph() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of Nodes: ");
+        n = sc.nextInt();
+        int maxEdges = n * (n - 1);
+        for (int i = 1; i <= maxEdges; i++) {
+            System.out.println("Enter edge (0 0 to quit): ");
+            int origin = sc.nextInt();
+            int destin = sc.nextInt();
 
-        public Node(int val) {
-            this.val = val;
-            this.visited = false;
+            if (origin == 0 && destin == 0) break;
+
+            if (origin > n || destin > n || origin <= 0 || destin <= 0) {
+                System.out.println("Invalid Edge");
+                i--;
+            } else {
+                adj[origin][destin] = 1;
+            }
         }
     }
 
-    // BFS function
-    public static void bfs(Node[] vertices, Node start) {
-        Queue<Node> queue = new LinkedList<>();
+    public static void display() {
+        System.out.println("Adjacency Matrix:");
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print(adj[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
 
-        // Mark the start node as visited and enqueue it
-        start.visited = true;
-        queue.add(start);
+    public static void bfs(int v) {
+        Queue<Integer> queue = new LinkedList<>();
+        System.out.print("BFS Traversal: " + v + " ");
+        visited[v] = true;
+        queue.add(v);
 
-        System.out.print("BFS Traversal: ");
-        
-        // Perform BFS
         while (!queue.isEmpty()) {
-            Node current = queue.poll(); // Dequeue the node
-            System.out.print((char) (current.val + 'A') + " "); // Print the node
-
-            // Visit all neighbors
-            for (int i = 0; i < NODE; i++) {
-                if (graph[current.val][i] == 1 && !vertices[i].visited) { // Edge exists and not visited
-                    vertices[i].visited = true; // Mark as visited
-                    queue.add(vertices[i]); // Enqueue the neighbor
+            v = queue.poll();
+            for (int i = 1; i <= n; i++) {
+                if (adj[v][i] == 1 && !visited[i]) {
+                    System.out.print(i + " ");
+                    visited[i] = true;
+                    queue.add(i);
                 }
             }
         }
-        System.out.println(); // New line after traversal
+        System.out.println();
     }
 
     public static void main(String[] args) {
-        System.out.println("Roll no. 60");
-        System.out.println("Graph Traversal using BFS.\n");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Roll no-9");
+        createGraph();
+        while (true) {
+            System.out.println("\n1. Breadth First Search Using Queue");
+            System.out.println("2. Exit");
+            System.out.print("Enter your choice: ");
+            int ch = sc.nextInt();
 
-        Node[] vertices = new Node[NODE];
-
-        // Initialize all vertices
-        for (int i = 0; i < NODE; i++) {
-            vertices[i] = new Node(i);
+            switch (ch) {
+                case 1:
+                    System.out.print("Enter Starting node for Breadth First Search: ");
+                    int v = sc.nextInt();
+                    Arrays.fill(visited, false); // Reset visited array
+                    bfs(v);
+                    break;
+                case 2:
+                    System.out.println("Exiting...");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Wrong Choice");
+                    break;
+            }
         }
-
-        // Starting node: B (index 1)
-        char startChar = 'B';
-        Node startNode = vertices[startChar - 'A'];
-
-        // Perform BFS starting from the given node
-        bfs(vertices, startNode);
     }
 }
-
-
